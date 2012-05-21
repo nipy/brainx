fde7626d2ba4d86c099821296f24ad7864a6a894
Caterina Gratton <cgratton@berkeley.edu>
2012-05-21 14:36:52 -0700

    Ensure tot_edges is int for range

--- brainx/util.py
+++ brainx/util.py
@@ -61,7 +61,6 @@ def format_matrix2(data,s,sc,c,lk,co,idc = [],costlist=[],nouptri = False):
 
 def cost_size(nnodes):
     tot_edges = .5*nnodes*(nnodes-1)
-    
     costs =  np.array(range(int(tot_edges)+1),dtype=float)/tot_edges
     edges_short = tot_edges/2
     return costs,tot_edges,edges_short
