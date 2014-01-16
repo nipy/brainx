4c5b322208e9457450aa35c19bd5783afbfb8cb6
CindeeM <cindee@berkeley.edu>
2014-01-16 14:37:41 -0800

    NF: initial import of weighted modularity and tests

---
+++ brainx/tests/test_weighted_modularity.py
@@ -0,0 +1,112 @@
+"""Tests for the weighted_modularity module"""
+
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+import os
+import unittest
+
+
+# Third party
+import networkx as nx
+import nose.tools as nt
+import numpy as np
+import numpy.testing as npt
+
+# Our own
+from .. import util
+from .. import weighted_modularity as wm
+
+
+def get_test_data():
+	""" grabs local txt file with adj matrices
+    Returns
+    =======
+    graph : networkx graph
+    community : list of sets
+    """
+	pth, _ = os.path.split(__file__)
+	testdir = os.path.join(pth, 'tdata_corr_txt')
+	data_file = os.path.join(testdir, '101_Block01.txt')
+	mat = np.loadtxt(data_file)
+	graph = nx.from_numpy_matrix(mat)
+	# graph has 85 nodes, make generic community
+	community = [set(range(42)), set(range(42,86))]
+	return graph, community
+
+class TestPartition(unittest.TestCase):
+
+    def setUp(self):
+        ## generate a default graph and community
+        graph, community = get_test_data()
+        self.graph = graph
+        self.community = community
+
+    def test_init(self):
+        part = wm.Partition(self.graph)
+        self.assertEqual(type(part.degrees), type({}))
+        npt.assert_array_almost_equal(part.total_edge_weight, 1485.466536)
+        self.assertEquals(part.graph, self.graph)
+        # generated communities
+        comm = [set([node]) for node in self.graph.nodes()]
+        self.assertEqual(part.community, comm)
+
+    def test_community_degree(self):
+        ## if no community, method will raise error
+        part = wm.Partition(self.graph)
+        part = wm.Partition(self.graph, self.community)
+        cdegree = part.community_degree()
+        self.assertEqual(round(cdegree[0]), 1444.0)
+
+
+    def test_set_community(self):
+        part = wm.Partition(self.graph, self.community)
+        self.assertEqual(part.community, self.community)
+        with self.assertRaises(TypeError):
+            # raise error if not list of sets
+            part.set_community(part.community[0])
+        with self.assertRaises(TypeError):
+            part.set_community('a')
+        with self.assertRaises(ValueError):
+            ## missing nodes
+            comm = self.graph.nodes()[:-3]
+            part.set_community([set(comm)])
+
+    def test_allnodes_in_community(self):
+        """checks communities contain all nodes
+        with no repetition"""
+        part = wm.Partition(self.graph)
+        self.assertTrue(part._allnodes_in_community(self.community))
+        self.assertFalse(part._allnodes_in_community([self.community[0]]))
+
+
+    def test_get_node_community(self):
+        part = wm.Partition(self.graph, self.community)
+        self.assertEqual(part.get_node_community(0), 0)
+        self.assertEqual(part.get_node_community(self.graph.nodes()[-1]),1)
+        with self.assertRaises(ValueError):
+            part.get_node_community(-1)
+        part = wm.Partition(self.graph)
+        self.assertEqual(part.get_node_community(0), 0)
+
+
+def test_modularity():
+    graph, comm = get_test_data()
+    part = wm.Partition(graph, comm)
+    npt.assert_almost_equal(wm.modularity(part), -0.00192480)
+
+def test_meta_graph():
+    graph, community = get_test_data()
+    part = wm.Partition(graph)
+    metagraph = wm.meta_graph(part)
+    ## each node is a comm, so no change to metagraph
+    npt.assert_equal(metagraph.nodes(), graph.nodes())
+    ## two communitties
+    part = wm.Partition(graph, community)
+    metagraph = wm.meta_graph(part)
+    npt.assert_equal(metagraph.nodes(), [0,1])
+    npt.assert_equal(metagraph.edges(), [(0,0),(0,1), (1,1)])
+    ## weight should not be lost between graphs
+    npt.assert_almost_equal(metagraph.size(weight='weight'),
+        graph.size(weight='weight'))
+
---
+++ brainx/weighted_modularity.py
@@ -0,0 +1,148 @@
+
+
+import numpy as np
+import networkx as nx
+from . import util
+
+
+class Partition:
+    def __init__(self, graph, community=None):
+        """ initialize partition of graph, with optional community
+        defined
+
+        Parameters
+        ==========
+        graph : networkx graph
+        community : list of lists
+            a list of lists with nodes in each list
+        """
+        self.graph = graph
+        if community is None:
+            self._community = self._init_communities_from_nodes()
+        else:
+            self.set_community(community)
+        self.total_edge_weight = graph.size(weight='weight')
+        self.degrees = graph.degree(weight = 'weight')
+
+    @property
+    def community(self):
+        return self._community
+    @community.setter
+    def community(self, value):
+        self._community = self.set_community(value)
+
+    def _init_communities_from_nodes(self):
+        """ creates a new community with one node per community
+        eg nodes = [0,1,2] -> community = [set([0]), set([1]), set([2])]
+        """
+        return [set([node]) for node in self.graph.nodes()]
+
+
+    def community_degree(self):
+        """ calculates the joint degree of a community"""
+        community_degrees = []
+        for com in self.community:
+            tmp = np.sum([self.graph.degree(weight='weight')[x] for x in com])
+            community_degrees.append(tmp)
+        return community_degrees
+
+    def get_node_community(self, node):
+        """returns the node's community"""
+        try:
+            return [val for val,x in enumerate(self.community) if node in x][0]
+        except:
+            if not node in self.graph.nodes():
+                raise ValueError('node {0} not in graph for this '\
+                    'partition'.format(node))
+            else:
+                raise StandardError('cannot find community for node '\
+                    '{0}'.format(node))
+
+    def set_community(self, community):
+        """ set the partition community to the input community"""
+        if self._allnodes_in_community(community):
+            self._community = community
+        else:
+            raise ValueError('missing nodes {0}'.format(community))
+
+
+    def _allnodes_in_community(self, community):
+        """ checks all nodes are represented in communities, also catches
+        duplicate nodes"""
+        if not (isinstance(community, list) and \
+            util._contains_only(community, set)):
+            raise TypeError('community should be list of sets, not'\
+                '{}'.format(community))  
+        ## simple count to check for all nodes
+        return len(self.graph.nodes()) == \
+            len([item for com in community for item in com])
+
+def _intersect_neighbors_community(part, node):
+    """for a given partition, and node find the other nodes with
+    edges to node in the community"""
+    graph = part.graph
+    neighbors = graph[node].keys()
+    node_community = part.get_node_community(node)
+    community = [x for x in part.community[node_community] if not x == node]
+    # remove node so we can check for self-loops (in neighbors)
+    intersect = list(set(neighbors) & set(community))
+    return intersect
+
+
+
+def modularity(partition):
+    """Modularity of a graph with given partition
+    using Newman 2004 Physical Review paper
+
+    Parameters
+    ==========
+    partition : weighted graph partition object
+
+    Returns
+    =======
+    modularity : float
+        value reflecting the relation of within community connection
+        to across community connections
+    """
+    if partition.graph.is_directed():
+        raise TypeError('only valid on non directed graphs')
+    graph = partition.graph
+    community_degree = partition.community_degree()
+    comm_within_weight = [0] * len(partition.community)
+    for node in graph:
+        within = _intersect_neighbors_community(partition, node)
+        inweight = 0
+        if node in within: # self loop
+            inweight += graph[node][node]['weight']
+            within.remove(node)
+        inweight += np.sum([graph[node][other]['weight'] \
+            for other in within]) / 2.0
+        comm_within_weight[partition.get_node_community(node)] += inweight
+
+    community_degree = np.array(partition.community_degree())
+    full_weight = np.array(partition.total_edge_weight)
+    modularity = np.sum((comm_within_weight / full_weight) - \
+        (community_degree / (2 * full_weight))**2)
+    return modularity
+
+def meta_graph(partition):
+    """ takes partition communitites and creates a new graph with communities
+    as nodes, this includes self-loops"""
+    metagraph = nx.Graph()
+    # new nodes are communities
+    newnodes = [val for val,_ in enumerate(partition.community)]
+    metagraph.add_nodes_from(newnodes, weight=0.0) 
+
+    for node1, node2, data in partition.graph.edges_iter(data=True):
+        node1_community = partition.get_node_community(node1)
+        node2_community = partition.get_node_community(node2)
+        try:
+            tmpw = metagraph[node1_community][node2_community]['weight']
+        except:
+            tmpw = 0
+        metagraph.add_edge(
+            node1_community,
+            node2_community,
+            weight = tmpw + data['weight'])
+
+    return metagraph
