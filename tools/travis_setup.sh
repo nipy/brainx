b796fd637a86484881ed1b55b3f87a32d4ef6189
Stefan van der Walt <stefan@sun.ac.za>
2014-10-16 03:22:18 +0200

    Speed up build slightly by providing system libzmq

--- tools/travis_setup.sh
+++ tools/travis_setup.sh
@@ -5,5 +5,6 @@ WHEELHOUSE="--no-index --find-links=http://wheels.scikit-image.org/"
 
 pip install wheel nose
 pip install -r requirements.txt $WHEELHOUSE
+apt-get install libzmq-dev
 pip install ipython runipy
 
