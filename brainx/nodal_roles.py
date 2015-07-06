0fe93ad0a91e911d5ac23de6659189a8d570e966
Katelyn Arnemann <katelyn.begany@berkeley.edu>
2015-07-06 14:29:26 -0700

    Update nodal_roles.py
    Adjust terminology to match Rubinov & Sporns, 2011
    weighted degree --> strength

--- brainx/nodal_roles.py
+++ brainx/nodal_roles.py
@@ -4,8 +4,9 @@ import numpy as np
 from random import choice
 import networkx as nx
 
-def within_community_degree(weighted_partition, edgeless = np.nan, catch_edgeless_node=True):
-    ''' Computes "within-module degree" (z-score) for each node (Guimera 2005, Nature)
+def within_community_strength(weighted_partition, edgeless = np.nan, catch_edgeless_node=True):
+    ''' Computes "within-module strength" (i.e. weighted degree) z-score for each node 
+    (Guimera 2005, Nature)
 
     ------
     Parameters
@@ -18,38 +19,38 @@ def within_community_degree(weighted_partition, edgeless = np.nan, catch_edgeles
         number to replace edgeless nodes with
         default = 0.0
     catch_edgeless_node: Boolean
-        raise ValueError if node degree is zero
+        raise ValueError if node strength is zero
         default = True
         saves wcd of these nodes as edgeless variable if False
 
     ------
     Returns
     ------
-    within_community_degree: dict
-        Dictionary of the within community degree of each node.
+    within_community_strength: dict
+        Dictionary of the within community strength of each node.
 
     '''
     wc_dict = {}
     for c, community in enumerate(weighted_partition.communities):
-        community_degrees = []
-        for node in community: #get average within-community-degree
-            node_degree = weighted_partition.node_degree(node)
-            if node_degree == 0.0: #catch edgeless nodes, this shouldn't count towards avg wcd
+        community_strengths = []
+        for node in community: #get average within-community strength (i.e. weighted degree)
+            node_strength = weighted_partition.node_strength(node)
+            if node_strength == 0.0: #catch edgeless nodes, this shouldn't count towards avg wcd
                 if catch_edgeless_node:
                     raise ValueError("Node {} is edgeless".format(node))
                 continue
-            community_degrees.append(weighted_partition.node_degree_by_community(node)[c])
-        std = np.std(community_degrees) # std of community's degrees
-        mean = np.mean(community_degrees) # mean of community's degrees
-        for node in community: #get node's within_community-degree z-score
-            if weighted_partition.node_degree(node) == 0:
+            community_strengths.append(weighted_partition.node_strength_by_community(node)[c])
+        std = np.std(community_strengths) # std of community's strengths
+        mean = np.mean(community_strengths) # mean of community's strengths
+        for node in community: #get node's within_community-strength z-score
+            if weighted_partition.node_strength(node) == 0:
                 wc_dict[node] = edgeless
                 continue
-            within_community_degree = weighted_partition.node_degree_by_community(node)[c]
+            within_community_strength = weighted_partition.node_strength_by_community(node)[c]
             if std == 0.0: #so we don't divide by 0
-                wc_dict[node] = (float(within_community_degree) - float(mean)) #z_score
+                wc_dict[node] = (float(within_community_strength) - float(mean)) #z_score
                 continue
-            wc_dict[node] = ((float(within_community_degree) - float(mean)) / std) #z_score
+            wc_dict[node] = ((float(within_community_strength) - float(mean)) / std) #z_score
     return wc_dict
 
 def participation_coefficient(weighted_partition, edgeless =np.nan, catch_edgeless_node=True):
@@ -64,7 +65,7 @@ def participation_coefficient(weighted_partition, edgeless =np.nan, catch_edgele
         weighted_partitions = louvain.run()
         weighted_partition = weighted_partition[0], where index is the partition level
     catch_edgeless_node: Boolean
-        raise ValueError if node degree is zero
+        raise ValueError if node strength is zero
         default = True
 
     ------
@@ -75,15 +76,15 @@ def participation_coefficient(weighted_partition, edgeless =np.nan, catch_edgele
     '''
     pc_dict = {}
     for node in weighted_partition.graph:
-        node_degree = weighted_partition.node_degree(node)
-        if node_degree == 0.0: 
+        node_strength = weighted_partition.node_strength(node)
+        if node_strength == 0.0: 
             if catch_edgeless_node:
                 raise ValueError("Node {} is edgeless".format(node))
             pc_dict[node] = edgeless
             continue    
         pc = 0.0
-        for community_degree in weighted_partition.node_degree_by_community(node):
-            pc = pc + ((float(community_degree)/float(node_degree))**2)
+        for community_strength in weighted_partition.node_strength_by_community(node):
+            pc = pc + ((float(community_strength)/float(node_strength))**2)
         pc = 1-pc
         pc_dict[node] = pc
     return pc_dict
