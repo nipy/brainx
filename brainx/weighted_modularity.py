5a6b0ec89d16db1025dcfbdaa390857cc6e9c4d7
Katelyn Arnemann <klarnemann@users.noreply.github.com>
2015-08-27 17:42:07 -0700

    Fix 'neg' & 'smp' modularity calcs in weighted_modularity.py

--- brainx/weighted_modularity.py
+++ brainx/weighted_modularity.py
@@ -260,8 +260,8 @@ class WeightedPartition(object):
             return np.sum(pos_win_community / pos_m2\
                               - (pos_tot_community / (2 * pos_m2)) ** 2)
         elif qtype == 'neg':
-            return - np.sum(neg_win_community / neg_m2\
-                              - (neg_tot_community / (2 * neg_m2)) ** 2)
+            return -(np.sum(neg_win_community / neg_m2\
+                              - (neg_tot_community / (2 * neg_m2)) ** 2))
         elif qtype == 'smp':
             q_pos = self.modularity(qtype='pos')
             q_neg = self.modularity(qtype='neg')
@@ -420,8 +420,8 @@ class LouvainCommunityDetection(object):
              return pos_node_community_strength - \
                  pos_totc * pos_node_strength / (pos_m * 2)
         elif self.qtype or arg == 'neg':
-             return - neg_node_community_strength - \
-                 neg_totc * neg_node_strength / (neg_m * 2)
+             return - (neg_node_community_strength - \
+                 neg_totc * neg_node_strength / (neg_m * 2))
         elif self.qtype == 'smp':
             pos_delta_modularity = self._calc_delta_modularity(node, part, 'pos')
             neg_delta_modularity = self._calc_delta_modularity(node, part, 'neg')
