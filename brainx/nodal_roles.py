69c2e212cd4194749cfe66bc0fe6086c3594fa13
klarnemann <klarnemann@berkeley.edu>
2015-09-14 15:50:25 -0700

    change options for nodal_roles.py, set default to pos

--- brainx/nodal_roles.py
+++ brainx/nodal_roles.py
@@ -6,7 +6,7 @@ import numpy as np
 from random import choice
 import networkx as nx
 
-def within_community_strength(weighted_partition, calc_type, edgeless = np.nan, catch_edgeless_node=True):
+def within_community_strength(weighted_partition, calc_type='pos', edgeless = np.nan, catch_edgeless_node=True):
     ''' Computes "within-module strength" (i.e. weighted degree) z-score for each node 
     (Guimera 2005, Nature)
 
@@ -70,7 +70,7 @@ def within_community_strength(weighted_partition, calc_type, edgeless = np.nan, 
         wc_dict.update(community_wc_dict)
     return wc_dict
 
-def participation_coefficient(weighted_partition, calc_type, edgeless =np.nan, catch_edgeless_node=True):
+def participation_coefficient(weighted_partition, calc_type='pos', edgeless =np.nan, catch_edgeless_node=True):
     '''
     Computes the participation coefficient for each node (Guimera 2005, Nature)
 
@@ -82,7 +82,6 @@ def participation_coefficient(weighted_partition, calc_type, edgeless =np.nan, c
         weighted_partitions = louvain.run()
         weighted_partition = weighted_partition[0], where index is the partition level
     calc_type : str
-        'all' = calculate participation coefficient on all weights
         'pos' = calculate participation coefficient on positive weights only
         'neg' = calculate participation coefficient on negative weights only
     catch_edgeless_node: Boolean
@@ -97,17 +96,14 @@ def participation_coefficient(weighted_partition, calc_type, edgeless =np.nan, c
     '''
     pc_dict = {}
     for node in weighted_partition.graph:
-        if calc_type == 'all':
-            node_strength = weighted_partition.node_strength(node)
-            node_community_strengths = weighted_partition.node_strength_by_community(node)
-        elif calc_type == 'pos':
+        if calc_type == 'pos':
             node_strength = weighted_partition.node_positive_strength(node)
             node_community_strengths = weighted_partition.node_positive_strength_by_community(node)
         elif calc_type == 'neg':
             node_strength = weighted_partition.node_negative_strength(node)
             node_community_strengths = weighted_partition.node_negative_strength_by_community(node)
         else:
-            raise ValueError('%s not supported; only all, pos, and neg options are supported.' % (calc_type))
+            raise ValueError('%s not supported; only pos and neg options are supported.' % (calc_type))
         if node_strength == 0.0: 
             if catch_edgeless_node:
                 raise ValueError("Node {} is edgeless".format(node))
@@ -115,7 +111,7 @@ def participation_coefficient(weighted_partition, calc_type, edgeless =np.nan, c
             continue    
         pc = 0.0
         for community_strength in node_community_strengths:
-            pc = pc + ((float(community_strength)/float(node_strength))**2)
+            pc += ((float(community_strength) / float(node_strength)) ** 2)
         pc = 1 - pc
         pc_dict[node] = pc
     return pc_dict
